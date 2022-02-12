ac4f90ef5e408e69663fcbb1be878119b75dcfc0
Christian Koop <contact@sprax2013.de>
2022-02-12 23:48:17 +0100

    Adds `StringValidator` class (+ tests)
    This class can be used to check if a given string **looks** like an HTTP-URL or an email address.
    The methods focus on performance while trying to be accurate. But most importatly, to not over-block either.

---
+++ src/strings/StringValidators.ts
@@ -0,0 +1,50 @@
+import Net from 'net';
+import StringUtils from './StringUtils';
+
+export const HOSTNAME_PATTERN = /(?=^.{4,253}$)(^((?!-)[a-z0-9-]{0,62}[a-z0-9]\.)+[a-z]{2,63}\.?$)/i;
+export const EMAIL_PATTERN = /^[a-z0-9.!#$%&'*+/="?^_`{|}~-]+@[a-z0-9](?:[a-z0-9-]{0,61}[a-z0-9])?(?:\.[a-z0-9](?:[a-z0-9-]{0,61}[a-z0-9])?)*$/i;
+
+export type HttpCheckResult = { valid: boolean, issue?: 'protocol' | 'port' | 'hostname' };
+
+export default class StringValidators {
+  public static looksLikeHttpUrl(url: string): HttpCheckResult {
+    // Protocol
+    if (!/^https?:\/\/.*$/i.test(url)) {
+      return {valid: false, issue: 'protocol'};
+    }
+
+    let lowerHost = url.toLowerCase().substring(url.indexOf('/') + 2);
+
+    // Path
+    if (lowerHost.indexOf('/') != -1) {
+      lowerHost = lowerHost.substring(0, lowerHost.indexOf('/'));
+    }
+
+    // Port
+    if (lowerHost.lastIndexOf(':') != -1) {
+      const portStr = lowerHost.substring(lowerHost.lastIndexOf(':') + 1);
+      const port = parseInt(portStr, 10);
+      lowerHost = lowerHost.substring(0, lowerHost.lastIndexOf(':'));
+
+      if (!StringUtils.isNumeric(portStr) || port <= 0 || port > 65535 /* unsigned int2 */) {
+        return {valid: false, issue: 'port'};
+      }
+    }
+
+    if (Net.isIPv4(lowerHost) || Net.isIPv6(lowerHost) || lowerHost == 'localhost') {
+      return {valid: true};
+    }
+
+    const valid = HOSTNAME_PATTERN.test(lowerHost);
+
+    if (valid) {
+      return {valid};
+    }
+
+    return {valid, issue: 'hostname'};
+  }
+
+  public static looksLikeValidEmail(email: string): boolean {
+    return EMAIL_PATTERN.test(email);
+  }
+}
---
+++ tests/strings/StringValidators.test.ts
@@ -0,0 +1,69 @@
+import StringValidators, { HttpCheckResult } from '../../src/strings/StringValidators';
+
+describe('Check if strings look like valid HTTP(s)-URLs', () => {
+  const valid: HttpCheckResult = {valid: true};
+
+  test('Test protocols', () => {
+    expect(StringValidators.looksLikeHttpUrl('htt' + 'p://example.com/'))
+        .toStrictEqual(valid);
+    expect(StringValidators.looksLikeHttpUrl('https://www.example.com/'))
+        .toStrictEqual(valid);
+
+    expect(StringValidators.looksLikeHttpUrl('sftp://www.example.com/'))
+        .toStrictEqual({valid: false, issue: 'protocol'});
+
+    expect(StringValidators.looksLikeHttpUrl('example.com/'))
+        .toStrictEqual({valid: false, issue: 'protocol'});
+  });
+
+  test('Test Hostnames (+ localhost, IPv4, punycode)', () => {
+    expect(StringValidators.looksLikeHttpUrl('https://www.example.com/'))
+        .toStrictEqual(valid);
+    expect(StringValidators.looksLikeHttpUrl('https://example.com/'))
+        .toStrictEqual(valid);
+    expect(StringValidators.looksLikeHttpUrl('https://localhost/'))
+        .toStrictEqual(valid);
+
+    expect(StringValidators.looksLikeHttpUrl('https://1.1.1.1/'))
+        .toStrictEqual(valid);
+
+    expect(StringValidators.looksLikeHttpUrl('https://xn--xmpl-loa1ab.com/'))
+        .toStrictEqual(valid);
+    expect(StringValidators.looksLikeHttpUrl('https://éxämplè.com/'))
+        .toStrictEqual({valid: false, issue: 'hostname'});
+  });
+
+  test('Test ports', () => {
+    expect(StringValidators.looksLikeHttpUrl('https://www.example.com:8080/'))
+        .toStrictEqual(valid);
+
+    expect(StringValidators.looksLikeHttpUrl('https://www.example.com:99999/'))
+        .toStrictEqual({valid: false, issue: 'port'});
+    expect(StringValidators.looksLikeHttpUrl('https://www.example.com:abc/'))
+        .toStrictEqual({valid: false, issue: 'port'});
+  });
+
+  test('With path and query parameters', () => {
+    expect(StringValidators.looksLikeHttpUrl('https://example.com/Hellû'))
+        .toStrictEqual(valid);
+    expect(StringValidators.looksLikeHttpUrl('https://example.com/Hell%C3%BB?foo=bar'))
+        .toStrictEqual(valid);
+  });
+});
+
+describe('Check if strings look like valid email addresses', () => {
+  const validAddresses = [
+    '0123456789@example.com', 'foo@bar.example.com', 'foobar@1.1.1.1', 'foobar@localhost', 'foobar@xn--xmpl-loa1ab.com',
+    'foo+bar@example.com', 'foo.bar@example.com', '_!*-?%~@example.com', '"foo"."bar"@example.com'
+  ];
+  const invalidAddresses = ['foobar@example,com', 'foo@example..com', 'example.com', 'foo@', 'foo@.com'];
+
+  it.each(validAddresses)('Valid address: %s', (address) => {
+    expect(StringValidators.looksLikeValidEmail(address))
+        .toBe(true);
+  });
+  it.each(invalidAddresses)('Invalid address: %s', (address) => {
+    expect(StringValidators.looksLikeValidEmail(address))
+        .toBe(false);
+  });
+});
