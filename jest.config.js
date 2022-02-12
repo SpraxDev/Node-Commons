6bd8eb487434af204e3988cf380bdcdb4e085d9d
Christian Koop <contact@sprax2013.de>
2022-02-13 00:24:48 +0100

    Initial project setup

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,7 @@
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
   preset: 'ts-jest',
-  testEnvironment: 'node'
+  testEnvironment: 'node',
+  clearMocks: true,
+  collectCoverageFrom: ['src/**/*.ts']
 };
